b38f531e4fe03e72d8b861cae90ee723b3122cea
phamlucchuong <lucchuongg@gmail.com>
2023-11-16 03:17:38 +0700

    làm buttonho thanh announce

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,18 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     const announcementBar = document.querySelector('.top-title');
+    const nextButton = document.querySelector('.next-button');
+    const prevButton = document.querySelector('.prev-button');
 
     function showAnnouncement() {
         announcementBar.classList.add('active');
-        setTimeout(hideAnnouncement, 10000); // Ẩn thanh thông báo sau 2 giây
+        setTimeout(hideAnnouncement, 2000); // Ẩn thanh thông báo sau 2 giây
     }
 
     function hideAnnouncement() {
         announcementBar.classList.remove('active');
-        setTimeout(showNextAnnouncement, 500); // Hiển thị thông báo tiếp theo sau 0.5 giây
+        // Không cần setTimeout ở đây vì sẽ được gọi ngay lập tức khi ẩn
+        showNextAnnouncement();
     }
 
     let announ = [
@@ -18,19 +22,37 @@ document.addEventListener('DOMContentLoaded', function () {
         'FREE SHIPPING VỚI HÓA ĐƠN TỪ 900K!'
     ];
 
-    let i = 0;
+    let currentIndex = 0;
 
     function showNextAnnouncement() {
-        announcementBar.querySelector('.announcement-text').textContent = announ[i];
+        announcementBar.querySelector('.announcement-text').textContent = announ[currentIndex];
+        // Hiển thị thanh thông báo mới
+        showAnnouncement();
+    }
+    let i = 0;
+
+    function showPrevAnnouncement() {
+        currentIndex = (currentIndex - 1 + announ.length) % announ.length;
+        announcementBar.querySelector('.announcement-text').textContent = announ[currentIndex];
+        // Hiển thị thanh thông báo mới
         i = (i + 1) % announ.length;
         if(i==4){
             i = 0;
         }
-
-        // Hiển thị thanh thông báo mới
         showAnnouncement();
     }
 
     // Bắt đầu hiển thị thanh thông báo
     showAnnouncement();
+
+    // Bắt sự kiện khi bấm nút "Next"
+    nextButton.addEventListener('click', function () {
+        currentIndex = (currentIndex + 1) % announ.length;
+        showNextAnnouncement();
+    });
+
+    // Bắt sự kiện khi bấm nút "Prev"
+    prevButton.addEventListener('click', function () {
+        showPrevAnnouncement();
+    });
 });
