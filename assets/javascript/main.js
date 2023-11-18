7df9290606ca13d798d80e1af1e723d6024aa30c
phamlucchuong <lucchuongg@gmail.com>
2023-11-18 23:57:26 +0700

    làm thêm yêu cầu của cô Dung

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -54,39 +54,88 @@ function changeImage(newImage) {
 }
 
 
-function toggleSizeDropdown() {
-    var sizeDropdown = document.getElementById('sizeDropdown');
+function toggleDropdown(id) {
+    var sizeDropdown = document.getElementById(id);
     if (sizeDropdown.style.display === 'block') {
         sizeDropdown.style.display = 'none';
     } else {
         sizeDropdown.style.display = 'block';
-    }
-}
-
-function toggleSlDropdown() {
-    var sizeDropdown = document.getElementById('slDropdown');
-    if (sizeDropdown.style.display === 'block') {
-        sizeDropdown.style.display = 'none';
-    } else {
-        sizeDropdown.style.display = 'block';
-    }
-}
-
-
-function cartOnclick(){
-    var readButton = document.getElementById('cart-onclick');
-    if (readButton.style.display === 'block') {
-        readButton.style.display = 'none';
-    } else {
-        readButton.style.display = 'block';
     }
 }
 
 function readMoreClick(){
     var readButton = document.getElementById('read-more');
-    if (readButton.style.display === 'block') {
-        readButton.style.display = 'none';
+    readButton.style.display = 'none';
+}
+
+function validateEmail() {
+    var email = document.getElementById('email-login-input-text').value;
+    const emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    // Kiểm tra xem email có đúng định dạng không
+    if (emailRegex.test(email)) {
+        alert('Cảm ơn bạn ! Chúng tôi sẽ sớm phản hồi');
     } else {
-        readButton.style.display = 'block';
+        alert('Định dạng email không hợp lệ');
+    }
+  }
+
+
+  function chonSize(size, id){
+    document.getElementById(id).textContent = size;  
+}
+
+
+function inputSoLuong(id){
+    var soLuong = document.getElementById(id).value;
+    if(soLuong < 0){
+        alert('Số lượng không hợp lệ !');
+    }
+}
+
+function tttt(){
+    var sl = document.getElementById('doi-so-luong').value;
+    if(sl < 0){
+        alert('Số lượng không hợp lệ !');
+    }
+     else {
+        // window.location.href = "../index.html";
+        alert('Thanh toán thành công, cảm ơn bạn đã ủng hộ Ananas !');
+    }
+}
+
+function resetGioHang(){
+    document.getElementById('gio-hang-cua-ban').textContent = ' ';
+}
+
+
+function checkThongTin(){
+    var maDonHang = document.getElementById('ma-don-hang').value;
+    var soDienThoai = document.getElementById('so-dien-thoai').value;
+    var mdhRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])/;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var phoneRegex = /^\d{10}$/;
+    if(maDonHang == '' || soDienThoai == ''){
+        alert('Vui lòng nhập đầy đủ thông tin!');
+    } else {
+        if(!mdhRegex.test(maDonHang)) {
+            alert('Mã đơn hàng không hợp lệ!');
+        } else if(!emailRegex.test(soDienThoai) && !phoneRegex.test(soDienThoai)) {
+            alert('Email / Số điện thoại không hợp lệ!');
+        } else alert('Đơn hàng của bạn sẽ sớm được giao!');
+    }
+}
+
+function xoaYeuThich(){
+    document.getElementById('yeu-thich').textContent = '';
+}
+
+function themGioHang(){
+    var sl = document.getElementById('inputSl').value;
+    var size = document.getElementById('pick_Size').value;
+    if(sl > 0 && size != '' ){
+        alert('Đã thêm vào giỏ hàng!');
+        // window.location.href = '../index.html';
+    } else {
+        alert('Vui lòng chọn Size và số lượng!');
     }
 }
